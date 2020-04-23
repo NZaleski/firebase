905d09dd6cc00b86f4e2a7421706635a8ed1cc6f
Nicholas Zaleski <nicholas.zaleski1@gmail.com>
2020-04-23 19:50:34 -0400

    Add collection from form submission

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const cafeList = document.querySelector('#cafe-list')
+const form = document.querySelector('#add-cafe-form')
 
 const renderCafe = function(doc) {
     let li = document.createElement('li')
     let name = document.createElement('span')
     let city = document.createElement('span')
+    let cross = document.createElement('div')
 
     li.setAttribute('data-id', doc.id)
     name.textContent = doc.data().name
     city.textContent = doc.data().city
+    cross.textContent = 'x'
 
     li.appendChild(name)
     li.appendChild(city)
+    li.appendChild(cross)
 
     cafeList.appendChild(li)
 }
+
 
 db.collection('cafes').get().then((querySnapshot) => {
     querySnapshot.forEach(doc => {
         renderCafe(doc)
     })
+})
+
+form.addEventListener('submit', (e) => {
+    console.log('e', e)
+    e.preventDefault()
+    db.collection('cafes').add({ 
+        name: form.name.value, 
+        city: form.city.value 
+    })
+    form.name.value = ''
+    form.city.value = ''
+
 })
